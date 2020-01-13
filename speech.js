c09ee89bb1aa0af4803f7d0251aad1f69f828d7f
talha <talha.6862@gmail.com>
2020-01-13 17:30:12 +0500

    3rd step

---
+++ speech.js
@@ -0,0 +1,61 @@
+var speech1 = 'First let us have a look at our players and tactics\n' +
+    '\n' +
+    'Click on the TACTICS link in the top menu.';
+
+var speech2 = 'Hello and welcome to the proud club Bannu RSC.\n' +
+    '\n' +
+    'I\'m Miguel Tutoriález, your guide into the world of Trophy Manager.\n' +
+    '\n' +
+    'I have created a schedule for today to get you started as a new manager. Press the START button to begin.';
+
+var speech3 = 'Your team is set for your first match.\n' +
+    '\n' +
+    'When you are ready to play press Continue.';
+
+var speech4 = 'A friendly against our local division rivals from Alemannia Durham has been arranged. Let us proceed to the match-center and see what is happening.';
+
+var speech5 = 'The match has now ended.\n' +
+    '\n' +
+    'In the bottom you can see all information from the game. Try to look at the Player Breakdown - you should then see how your players did. Rating of 10 is a superb performance, while 1 is horrible.\n' +
+    '\n' +
+    'When ready to proceed click Continue.';
+
+var speech6 = 'Congratulations on completing the first step of the tutorial. You will now get 2,000,000 and 1  TM Pro days.\n' +
+    '\n' +
+    'Let us proceed to the next task of the day.';
+
+var speech7 = 'It seems your stadium needs a bit of work to make the fans happy.\n' +
+    '\n' +
+    'Press the START button.';
+
+var speech8 = 'Upgrade your Youth Academy on the list below the picture of your stadium grounds.';
+
+var speech9 = 'Excellent. Now we can focus on the squad\'s development. Our youth coach gave me a hint in his weekly report, that we have a potential future star in our youth team.\n' +
+    '\n' +
+    'Let us go to the YOUTH DEVELOPMENT section and have a look.';
+
+var speech10 = 'This is the Youth Development page. Every Saturday you can get a scout report on one or more youths who you can hire.\n' +
+    '\n' +
+    'Let\'s hire that little diamond in the rough';
+
+var speech11 = 'Now let us have a look at the training.\n' +
+    '\n' +
+    'Press START to begin.';
+
+var speech12 = 'Go to your TRAINING page to place our newly signed talent, Sterling Stockton, on a training team.';
+
+var speech13 = 'Your new talent (Sterling Stockton) is placed on a training team. You can drag him and your other players between your training teams so they can improve the appropriate skills. Goalkeepers will always train goalkeeping skills.\n' +
+    '\n' +
+    'Try moving a few players to different teams.\n' +
+    '\n' +
+    'Press Continue to go on.\n';
+
+var speech14 = 'Your team coach has arranged an extra training session for your new player.\n' +
+    '\n' +
+    'Go to the Player Profile of Sterling Stockton to see how he did.';
+
+var speech15 = 'Excellent! Sterling Stockton seems to be improving really well! In general younger players improve more than the older ones - but I bet you already knew that?\n' +
+    '\n' +
+    'The coach will have a new training report for you each Tuesday.\n' +
+    '\n' +
+    'Press Continue when you\'re ready.'
